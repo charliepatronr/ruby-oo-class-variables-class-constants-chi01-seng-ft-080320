907d402982b4897c39ca1ecf31feb50450986a87
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-08-05 14:59:58 -0500

    Done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,27 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS =  []
+
   def initialize(brand)
     @brand = brand
+    if(!BRANDS.include?(brand))
+      BRANDS << brand
+    end 
+
+  end
+
+
+  ## Test suite passes without if codition 
+  ## however if a shoe brand is modified and has not been initialized it should added to the BRANDS arr
+  def brand= (brand)
+    @brand = brand
+    if(!BRANDS.include? (brand))
+      BRANDS << brand
+    end 
   end
 
   def cobble
@@ -12,3 +30,14 @@ class Shoe
   end
 
 end
+
+
+brands = ["Uggs", "Rainbow", "Nike", "Nike"]
+
+shoe1 = Shoe.new("Uggs")
+shoe2 = Shoe.new("Rainbow")
+shoe4 = Shoe.new("Nike")
+shoe3 = Shoe.new("Nike")
+
+
+
